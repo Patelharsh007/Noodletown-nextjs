ab8db6d20369084181ae5c8579b666481f03fa43
Patelharsh007 <ptlharsh3073@gmail.com>
2025-05-13 16:13:27 +0530

    removed unused file

--- src/app/(other)/menu/loading.tsx
+++
@@ -1,9 +0,0 @@
-// app/(other)/menu/loading.tsx
-export default function MenuLoading() {
-  return (
-    <div>
-      <h1>Loading Menu...</h1>
-      <p>Please wait while we load the menu content.</p>
-    </div>
-  );
-}
